3e0341d3d5773ea5fc1d207b6a40501cbb89e954
nanato12 <admin@nanato12.info>
2021-10-18 12:51:26 +0900

    update: bot.views

--- apps/bot/views.py
+++ apps/bot/views.py
@@ -1,5 +1,18 @@
+import os
+from typing import List
+
 from django.http import HttpRequest, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from linebot.models.events import Event
+from linebot.webhook import WebhookParser
 
 
+@csrf_exempt
 def callback(request: HttpRequest) -> HttpResponse:
+    parser = WebhookParser(os.environ["LINE_CHANNEL_SECRET"])
+    events: List[Event] = parser.parse(
+        request.body.decode("utf-8"), request.headers["X-Line-Signature"]
+    )
+    for event in events:
+        print(event)
     return HttpResponse("OK")
