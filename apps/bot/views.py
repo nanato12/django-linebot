2679c89ce42a2afdc91ef143373f6182e4fa8f72
nanato12 <admin@nanato12.info>
2021-10-18 14:05:36 +0900

    update: bot.views

--- apps/bot/views.py
+++ apps/bot/views.py
@@ -7,6 +7,7 @@ from linebot import LineBotApi
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import TextSendMessage
 from linebot.models.events import MessageEvent, TextMessage
+from linebot.models.sources import SourceGroup, SourceRoom, SourceUser
 from linebot.webhook import WebhookHandler
 
 handler = WebhookHandler(os.environ["LINE_CHANNEL_SECRET"])
@@ -31,10 +32,26 @@ def text_message(event: MessageEvent) -> None:
     # message
     message: TextMessage = event.message
 
+    # profile
+    if isinstance(event.source, SourceUser):
+        profile = bot.get_profile(event.source.user_id)
+    elif isinstance(event.source, SourceGroup):
+        profile = bot.get_group_member_profile(
+            event.source.group_id, event.source.user_id
+        )
+    elif isinstance(event.source, SourceRoom):
+        profile = bot.get_room_member_profile(
+            event.source.room_id, event.source.user_id
+        )
+    else:
+        raise Exception("Invalid event.source type")
+
     if message.text == "メニュー":
         send_message = TextSendMessage("メニュー")
     elif message.text == "完了":
         send_message = TextSendMessage("注文を受付ました。")
+    elif message.text == "なまえ":
+        send_message = TextSendMessage(profile.display_name)
     else:
         send_message = TextSendMessage("テストメッセージ")
 
