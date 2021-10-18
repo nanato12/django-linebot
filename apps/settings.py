dc2fe56b385c3404ce439120f7e9a4936fbd947c
nanato12 <admin@nanato12.info>
2021-10-18 12:19:42 +0900

    update: settings

--- apps/settings.py
+++ apps/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS: List[str] = []
+ALLOWED_HOSTS: List[str] = ["*"]
 
 
 # Application definition
